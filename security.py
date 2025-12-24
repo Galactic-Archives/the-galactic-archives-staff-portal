404f2ce6ffb0940baa1fa0a74b3601a535b80089
Galactic Archives Hackathon <admiral@galacticarchives.space>
2025-12-24 22:18:34 +0000

    Change password hashing scheme to argon2

--- security.py
+++ security.py
@@ -4,7 +4,8 @@ import jwt
 from passlib.context import CryptContext
 from config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
 
 
 def hash_password(password: str) -> str:
